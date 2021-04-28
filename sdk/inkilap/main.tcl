33842092d9c0de960809d442ae4d740c997484ab
Yiğit Cemal Öztürk <69117901+CadmiumC4@users.noreply.github.com>
2021-04-28 08:06:50 +0300

    Update main.tcl

--- sdk/inkilap/main.tcl
+++ sdk/inkilap/main.tcl
@@ -1,2 +1,17 @@
 
-//I don't even know TCL.
+# I don't even know TCL.
+puts "Hello, World - In quotes"    ;# This is a comment after the command.
+
+# This is a comment at beginning of a line
+
+puts {Hello, World - In Braces}
+
+puts {Bad comment syntax example}  ; # *Error* - there is no semicolon!
+
+puts "This is line 1"; puts "this is line 2"
+
+puts "Hello, World; - With  a semicolon inside the quotes"
+
+# Words don't need to be quoted unless they contain white space:
+
+puts HelloWorld
