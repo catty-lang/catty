61d1cf469841918c0592fc254d1a56a8753039aa
Yiğit Cemal Öztürk <69117901+CadmiumC4@users.noreply.github.com>
2021-10-16 13:49:30 +0300

    Update memory.h

--- stdlib/core/ownership/memory.h
+++ stdlib/core/ownership/memory.h
@@ -1,12 +1,3 @@
-#import <Foundation.h>
 
-@interface CattyMemory: NSObject
-//Allocate memory
--(void*)malloc(unsigned size);
-
-//Free memory
--(void*)free(void *block);
-
-//Move ownership
--(void*)memmove(void *src, void *dst, int size);
-@end
+void *memmove(void *dest, const void *src, size_t n);
+void *malloc(
