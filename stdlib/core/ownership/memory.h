584e9112ae6f72215c7233ee3cd766ebbe0added
Yiğit Cemal Öztürk <69117901+CadmiumC4@users.noreply.github.com>
2021-10-18 07:44:23 +0300

    Delete memory.h

--- stdlib/core/ownership/memory.h
+++
@@ -1,7 +0,0 @@
-#include <stddef.h>
-#define CATTY_LANG_STDLIB 1
-#define CATTY_LANG_INTERNAL 1
-
-void *memmove(void *dest, const void *src, size_t n);
-void *malloc(size_t size);
-void free(void *ptr);
